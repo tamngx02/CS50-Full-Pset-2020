a398e116c705de73ccf4f341ec1a06e25a258fb7
tamngx02 <62319881+tamngx02@users.noreply.github.com>
2020-09-06 21:33:40 +0700

    Update mario.c

--- Pset-1/mario.c
+++ Pset-1/mario.c
@@ -1,1 +1,28 @@
-hello
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    int n;
+    // take the input
+    do
+    {
+        n = get_int ("Height: ");
+    } while (n < 1 || n > 8);
+   
+   
+    for (int i = 0; i < n; i++)
+    {
+        // for the space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            printf (" ");
+        }
+        // for the hashes
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf ("#");
+        }
+        printf ("\n");
+    }
+}
